70b4d017ecbeff3eaa580dfd4fc40f8f24542ba7
Regis Fischer <regis.fischer2021@campus-eni.fr>
2021-08-24 08:54:46 +0200

    #tp1-2 premier commit : avec tp1 et 2

---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/main", name="main")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('main/home.html.twig',[]);
+    }
+}
