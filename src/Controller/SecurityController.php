f566001412ae8d394b9418e120cb8f7f294bc6b4
Regis Fischer <regis.fischer2021@campus-eni.fr>
2021-08-30 09:14:04 +0200

    tp 8 ajout Securite et formualire login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils,UserPasswordEncoderInterface $passwordHasher,EntityManagerInterface $em): Response
+    {/*
+        $user=  new User();
+        $user->setUsername("rfischer")
+            ->setEmail("regis.fischer@gmail.com")
+            ->setPassword($passwordHasher->hashPassword($user,'pass'));
+        $em->persist($user);
+        $em->flush();
+    */
+    /*
+         if ($this->getUser()) {
+
+            return $this->redirectToRoute('wish_list');
+        }
+*/
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
